b16b515b575d10be7d219950c75bdf6978c3dbd5
Diego Moraes <diegofrost96@gmail>
2023-12-19 14:38:45 -0300

    first commit

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure commodi debitis minus earum sint eum, amet tenetur, expedita, veritatis perspiciatis unde tempore repellat totam aliquid! Magnam consequatur minus facere asperiores.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
